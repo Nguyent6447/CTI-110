be86e06d26c891e9e5e645fe4284340ce7007744
Luke Nguyen <146522150+Nguyent6447@users.noreply.github.com>
2023-11-07 11:21:57 -0500

    First Upload

---
+++ P1LAB1_NguyenTan.py
@@ -0,0 +1,12 @@
+#Tan Nguyen
+#11/07/2023
+#Basic Input/Output
+
+#Get input from user
+name = input ("Please enter your name: ")
+
+#Display Output to the user
+    
+print ("Hello", name, ", welcome to CTI-110!")
+
+
